5747e0dfb01125afc689ab3e51c1e1c7dc1589b2
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2021-11-02 05:46:35 +0700

    model transaction dan transaction detail

---
+++ app/Models/Transaction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['uuid', 'name', 'email', 'number', 'addres', 'transaction_total', 'transaction_status'];
+
+    protected $hidden = [];
+
+    public function details(){
+        return $this->hasMany(TransactionDetail::class, 'transactions_id');
+    }
+}
